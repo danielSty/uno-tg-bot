446e65f0c6862f2982bf3d343273e6b070e9b0a7
danielSty <60923370+danielSty@users.noreply.github.com>
2020-08-14 15:01:57 +0800

    Update uno.py

--- uno.py
+++ uno.py
@@ -7,7 +7,7 @@ from emoji import emojize
 
 
 group=00000
-bot = telebot.TeleBot("923980103:AAH8qayGZgKB8n10ykPctc0HsCOsrGwutwo")
+bot = telebot.TeleBot({tg bot token})
 player={}
 gameongoing=False
 deck=['red_0', 'red_1', 'red_2', 'red_3', 'red_4', 'red_5', 'red_6', 'red_7', 'red_8', 'red_9', 'red_+2', 'red_reverse', 'red_skip', 'blue_0', 'blue_1', 'blue_2', 'blue_3', 'blue_4', 'blue_5', 'blue_6', 'blue_7', 'blue_8', 'blue_9', 'blue_+2', 'blue_reverse', 'blue_skip', 'yellow_0', 'yellow_1', 'yellow_2', 'yellow_3', 'yellow_4', 'yellow_5', 'yellow_6', 'yellow_7', 'yellow_8', 'yellow_9', 'yellow_+2', 'yellow_reverse', 'yellow_skip', 'green_0', 'green_1', 'green_2', 'green_3', 'green_4', 'green_5', 'green_6', 'green_7', 'green_8', 'green_9', 'green_+2', 'green_reverse', 'green_skip', 'red_1', 'red_2', 'red_3', 'red_4', 'red_5', 'red_6', 'red_7', 'red_8', 'red_9', 'red_+2', 'red_reverse', 'red_skip', 'blue_1', 'blue_2', 'blue_3', 'blue_4', 'blue_5', 'blue_6', 'blue_7', 'blue_8', 'blue_9', 'blue_+2', 'blue_reverse', 'blue_skip', 'yellow_1', 'yellow_2', 'yellow_3', 'yellow_4', 'yellow_5', 'yellow_6', 'yellow_7', 'yellow_8', 'yellow_9', 'yellow_+2', 'yellow_reverse', 'yellow_skip', 'green_1', 'green_2', 'green_3', 'green_4', 'green_5', 'green_6', 'green_7', 'green_8', 'green_9', 'green_+2', 'green_reverse', 'green_skip','black_轉色','black_轉色','black_轉色','black_轉色','black_轉色+4','black_轉色+4','black_轉色+4','black_轉色+4']
